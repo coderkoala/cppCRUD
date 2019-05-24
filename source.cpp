47ae03be06f2ad15d61e5c4e1bc0315c3d921609
coderkoala <iamtribulation@outlook.com>
2019-05-24 20:27:18 +0545

    Added sqlite3 + beginning ORM next cycle

--- source.cpp
+++ source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<sqlite3.h>
 #define log(x) std::cout<<x<<endl;
 
 class coords{
@@ -26,7 +27,16 @@ std::ostream& operator<<(std::ostream& out, const coords &param)
 
 
 int main(){
-	
+	sqlite3* DB;
+	int exit = 0;
+	exit = sqlite3_open("example.db", &DB);
+	if(exit){
+		std::cout<<"Exited due to: "<<sqlite3_errmsg(DB)<<std::endl;
+		return 0;
+		
+	}
+	else std::cout<<"Database opened successfully"<<std::endl;
+	sqlite3_close(DB);
 	std::vector<coords> cords;
 	cords.push_back(coords(1,2,3));
 	cords.push_back(coords(5,6,7));
